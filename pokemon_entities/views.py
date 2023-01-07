3585a7fc2a15989b12730b8378bb253cc8731c6e
Nikolay Savchenko <7548568@gmail.com>
2023-01-07 22:25:44 +0300

    Fixed typo

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,4 @@
 import folium
-import requests
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.utils.timezone import localtime
